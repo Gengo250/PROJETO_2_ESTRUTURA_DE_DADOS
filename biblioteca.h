58d953f7484e0f0f2f810c3c60262c0f78369db1
Gengo250 <miguelgengo28@gmail.com>
2025-11-11 14:27:10 -0300

    Começo do projeto

---
+++ biblioteca.h
@@ -0,0 +1,31 @@
+#ifndef Biblioteca_Include
+#define Biblioteca_Include
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct no{
+  Sistema sistema;
+  struct no *esquerda, *direita;
+}NoArv;
+
+typedef struct {
+  NoArv *raiz;
+}ArvB;
+
+typedef struct data{
+  int dia, mes, ano;
+} Data;
+
+typedef struct Sistema{
+  char Cliente[50];
+  int ID;
+  int valor_transacao;
+  int matricula_do_vendedor;
+  char Vendedor[50];
+  Data data_transacao;
+} Sistema;
+
+
+
+
+#endif
